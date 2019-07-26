1a7fc799b7190588fc4aac8cae54bbe621683bb4
hacktivist123 <akintayoshedrack@gmail.com>
2019-07-26 18:35:26 +0100

    created Profile update and creation end points

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -73,7 +73,7 @@ router.post('/', [
     if (website) profileFields.website = website;
     if (location) profileFields.location = location;
     if (bio) profileFields.bio = bio;
-    if (status) profileFields.status = stus;
+    if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
       profileFields.skills = skills.split(',').map(skill => skill.trim());
@@ -87,10 +87,36 @@ router.post('/', [
     if (linkedin) profileFields.social.linkedin = linkedin;
     if (instagram) profileFields.social.instagram = instagram;
 
+    try {
+      let profile = await Profile.findOne({
+        user: req.user.id
+      })
 
-    console.log(profileFields.social);
+      if (profile) {
+        // Update
+        profile = await Profile.findOneAndUpdate({
+            user: req.user.id
+          }, {
+            $set: profileFields
+          }, {
+            new: true
+          }
 
-    res.send('welcome');
+        );
+        return res.json(profile);
+      }
+
+      // Create
+
+      profile = new Profile(profileFields);
+      await profile.save();
+
+      res.json(profile)
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error')
+    };
   }
 );
 
