5a863e2fe8102791e44e137097b2e164e030d6c0
hacktivist123 <akintayoshedrack@gmail.com>
2019-07-30 19:29:49 +0100

    created Delete User and Profile end point

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -161,7 +161,34 @@ router.get('/user/:user_id', async (req, res) => {
     }
     res.status(500).send('Cannot Get Profiles');
   }
-})
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id
+    });
+    // Remove User
+    await User.findOneAndRemove({
+      _id: req.user.id
+    });
+    res.json({
+      msg: 'User Deleted'
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Cannot Get Profile');
+
+  }
+});
+
 
 
 module.exports = router;
